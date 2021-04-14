951944292041c7c1941f2b1003cb2522ba1d269b
surojitfromindia <30839427+surojitfromindia@users.noreply.github.com>
2021-04-14 23:12:55 +0530

    Update API.js

--- src/controllers/api.js
+++ src/controllers/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://192.168.0.5:5000",
-  timeout: 2000,
+  baseURL: "https://vxback.herokuapp.com",
+  timeout: 3000,
 });
